7fec437167572557b2f940f3eb93b027fec8cf75
Alexander Hurtado <alexanderjhurtado@gmail.com>
2022-03-01 21:51:28 -0800

    Add heuristic metrics

--- what-they-said/src/data/benchmarking/compute_metrics.py
+++ what-they-said/src/data/benchmarking/compute_metrics.py
@@ -3,8 +3,7 @@ import json
 COMPUTED_DATA_FILEPATH = "./mini_predictions.json"
 LABEL_DATA_FILEPATH = "./mini_labels.json"
 METRIC_SAVE_FILEPATH = "./metrics.json"
-
-
+NUM_SIG_FIGS = 3
 
 with open(COMPUTED_DATA_FILEPATH, "r") as f:
     PREDICTED_DATA = json.load(f)
@@ -27,24 +26,44 @@ def report_attribution_metrics():
     predicted_attributions = get_all_attributions(PREDICTED_DATA)
     return compute_metrics(true_attributions, predicted_attributions)
 
+def report_heuristic_metrics():
+    total_quotes_detected = 0
+    heuristic_metrics = {}
+    for heuristic in ['pattern_1', 'pattern_2', 'pattern_3', 'pattern_4', 'proximity']:
+        precision, num_of_examples = compute_heuristic_metrics(heuristic)
+        total_quotes_detected += num_of_examples
+        heuristic_metrics[heuristic] = { 'precision': precision, 'num_of_examples': num_of_examples }
+    for heuristic, metrics in heuristic_metrics.items():
+        num_of_examples = metrics.pop('num_of_examples')
+        share_of_total = round(float(num_of_examples / total_quotes_detected), NUM_SIG_FIGS)
+        metrics['share_of_total'] = round(share_of_total * 100, NUM_SIG_FIGS)
+    return heuristic_metrics
+
 def compute_metrics(true_labels, detected_labels):
     precision = compute_precision(true_labels, detected_labels)
     recall = compute_recall(true_labels, detected_labels)
     f1_score = compute_f1_score(precision, recall)
     return { "precision": precision, "recall": recall, "f1_score": f1_score }
 
+def compute_heuristic_metrics(heuristic_filter):
+    true_attributions = get_all_attributions(TRUE_LABELS)
+    predicted_attributions = get_all_attributions(PREDICTED_DATA, heuristic_filter)
+    precision = compute_precision(true_attributions, predicted_attributions)
+    num_of_examples = len(predicted_attributions)
+    return precision, num_of_examples
+
 def compute_precision(true_labels, detected_labels):
     num_true_detected = get_num_true_detected(true_labels, detected_labels)
     num_detected = len(detected_labels)
-    return float(num_true_detected / num_detected)
+    return round(float(num_true_detected / num_detected), NUM_SIG_FIGS)
 
 def compute_recall(true_labels, detected_labels):
     num_true_detected = get_num_true_detected(true_labels, detected_labels)
     num_true = len(true_labels)
-    return float(num_true_detected / num_true)
+    return round(float(num_true_detected / num_true), NUM_SIG_FIGS)
 
 def compute_f1_score(precision, recall):
-    return (2 * precision * recall) / (precision + recall)
+    return round((2 * precision * recall) / (precision + recall), NUM_SIG_FIGS)
 
 def get_num_true_detected(true_labels, detected_labels):
     true_labels = true_labels.copy()
@@ -88,6 +107,12 @@ if __name__ == "__main__":
     quote_data = report_quote_metrics()
     entity_data = report_entity_metrics()
     attr_data = report_attribution_metrics()
+    heuristic_data = report_heuristic_metrics()
     with open(METRIC_SAVE_FILEPATH, "w") as f:
-        metric_data = {'quote_metrics': quote_data, 'entity_metrics': entity_data, 'attribution_metrics': attr_data}
+        metric_data = {
+            'quote_metrics': quote_data, 
+            'entity_metrics': entity_data, 
+            'attribution_metrics': attr_data,
+            'heuristic_metrics': heuristic_data
+        }
         json.dump(metric_data, f, ensure_ascii=False, indent=2)
