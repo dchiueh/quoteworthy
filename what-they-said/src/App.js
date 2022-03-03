10a15888218cd07a07a6ee4c2f5837f6f52f03fb
dchiueh <dchiueh@gmail.com>
2022-03-03 13:40:09 -0800

    Accessibility caption, title styling

--- what-they-said/src/App.js
+++ what-they-said/src/App.js
@@ -54,12 +54,13 @@ function App() {
   return (
     <Box className="Quoteworthy Application" sx={{margin: "auto", textAlign: "center"}}>
       <header style={{textAlign: "center"}}>
-        <h1 style={{fontFamily: 'Chomsky', fontSize:"40px", fontWeight:"200", marginBottom:"0", paddingBottom:"0", letterSpacing: "-0.5px"}}>Quoteworthy </h1>
+        <h1 style={{fontFamily: 'Chomsky', fontSize:"60px", fontWeight:"200", marginBottom:"0", paddingBottom:"0", marginTop:"20px", letterSpacing: "-0.5px"}}>Quoteworthy </h1>
         <p style={{marginTop:"10px"}}>Search our archive of <i>New York Times</i> articles by keyword to find relevant quotes sorted by speaker.</p> 
       </header>
             
       <HomeScreen />
       {/*TODO: fix code so we handle state a hell of a lot better */}
+      <p>Accessibility Caption: We present quoteworthy, a tool that extract quotes from an archive of 600 articles from the New York Times from 2018-2020. There is a search tool at the top that lets a user query based on keyword, and then relevant and correctly attributed quotes from our archive will populate. Filters by date and location are included as well. </p>
     </Box>
   );
 }
